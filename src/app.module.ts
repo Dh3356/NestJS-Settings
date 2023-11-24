420709784332663229398d2183c405fac04381ce
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-25 00:09:34 +0900

    feat: typeorm으로 연결, 테스트 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { validationSchema } from './config/validationSchema';
+import { ConfigModule } from '@nestjs/config';
+import { generateTypeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev'
+          ? '.env.dev'
+          : process.env.NODE_ENV === 'prod'
+            ? '.env.prod'
+            : '.env.test',
+      validationSchema,
+    }),
+    TypeOrmModule.forRoot(generateTypeOrmConfig(process.env)),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
