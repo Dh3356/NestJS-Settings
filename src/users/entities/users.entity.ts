973455badfffe9101953664a46a9feaeee4b851a
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-25 14:29:43 +0900

    refactor: 필요없는 컬럼, hashing 제거

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,26 +1,10 @@
-import { InternalServerErrorException } from '@nestjs/common';
-import {
-  BeforeInsert,
-  BeforeUpdate,
-  Column,
-  Entity,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
-import { IsEmail, IsString, Matches } from 'class-validator';
-import * as bcrypt from 'bcrypt';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsString, Matches } from 'class-validator';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id!: number;
-
-  @Column()
-  @IsString()
-  nickname!: string;
-
-  @Column({ unique: true })
-  @IsEmail()
-  email!: string;
 
   @Column({ select: false })
   @IsString()
@@ -29,24 +13,4 @@ export class User {
       'Password must be at least 8 characters(en) long, contain 1 number',
   })
   password!: string;
-
-  @BeforeInsert()
-  @BeforeUpdate()
-  async hashPassword(): Promise<void> {
-    if (this.password) {
-      try {
-        this.password = await bcrypt.hash(this.password, 10);
-      } catch (e) {
-        throw new InternalServerErrorException();
-      }
-    }
-  }
-
-  async checkPassword(plainPassword: string): Promise<boolean> {
-    try {
-      return await bcrypt.compare(plainPassword, this.password);
-    } catch (e) {
-      throw new InternalServerErrorException();
-    }
-  }
 }
